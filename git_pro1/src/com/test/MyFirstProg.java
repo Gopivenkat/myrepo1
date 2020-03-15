5555fd7145c3459738fad756d019f44cbead6a43
Gopi <gopikrishnanmkce@gmail.com>
2020-03-15 19:20:03 +0530

    elemntCode

--- git_pro1/src/com/test/MyFirstProg.java
+++ git_pro1/src/com/test/MyFirstProg.java
@@ -1,11 +1,34 @@
 package com.test;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class MyFirstProg {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world my program.");
-		System.out.println("How are you..");
+		String input="Hello How Are You ";
+		
+		
+		List<String> originalList=Arrays.asList(input.split(" "));
+		List<String> tempList=originalList;
+		
+		int index=0;
+		StringBuilder sb=null;
+		for(String s:originalList)
+		{
+			sb=new StringBuilder();
+			for(int i=index;i<tempList.size();i++)
+			{
+				sb.append(tempList.get(i)+"-");
+				//sb.append("-");
+					
+			}
+			
+			System.out.println(sb);
+			index++;
+		}
 
 	}
 
